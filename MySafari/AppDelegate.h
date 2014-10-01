7c60d4db46d0457abfec045f29795fb2e0e6721a
B2MW <P5@Champion.local>
2014-10-01 13:15:45 -0500

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by Bradley Walker on 10/1/14.
+//  Copyright (c) 2014 BlackSummerVentures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySafari/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySafari
+//
+//  Created by Bradley Walker on 10/1/14.
+//  Copyright (c) 2014 BlackSummerVentures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
